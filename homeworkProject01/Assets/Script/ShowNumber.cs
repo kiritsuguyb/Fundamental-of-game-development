5a72740d542d4175f1ca5bf3f2d91b5b0aa2af31
kiritsuguyb <1125010742@qq.com>
2020-09-21 12:17:33 +0800

    整理项目文件

---
+++ homeworkProject01/Assets/Script/ShowNumber.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowNumber : MonoBehaviour
+{
+    public List<GameObject> objects;
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = 0.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = objects.Count.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("SpawnedObj"))
+        {
+            if (!objects.Contains(other.gameObject))
+            {
+                objects.Add(other.gameObject);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("SpawnedObj"))
+        {
+            if (objects.Contains(other.gameObject))
+            {
+                objects.Remove(other.gameObject);
+            }
+        }
+    }
+
+}
---
+++ homeworkProject01/Assets/Script/ShowScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowScore : MonoBehaviour
+{
+    public PlayerNO player;
+    Score score;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = FindObjectOfType<Score>();
+        if (player == PlayerNO.PlayerA)
+            GetComponent<TextMeshProUGUI>().text = score.scoreA.ToString();
+        else
+            GetComponent<TextMeshProUGUI>().text = score.scoreB.ToString();
+    }
+}
