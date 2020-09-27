e39fed0159fc8d5a1b2c0e256278661aed820766
kiritsuguyb <1125010742@qq.com>
2020-09-27 18:51:11 +0800

    homework02 updated

---
+++ homework02/homeworkProject-XLua/Assets/LuaTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class LuaTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaEnv luaEnv = new LuaEnv();
+        luaEnv.DoString("CS.UnityEngine.Debug.Log('Hello world!')");
+        luaEnv.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
