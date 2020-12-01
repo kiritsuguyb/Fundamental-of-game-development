1c8e0b37104ebcb3429357de08d77d879e9edbcf
kiritsuguyb <1125010742@qq.com>
2020-12-01 16:46:19 +0800

    作业2 脚本语言基础 重新提交

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 本仓库是互动媒体开发基础课程作业。
 
-`homeworkProject-StateMachine`文件夹中是本次作业第四题，实现状态机来模拟一天的生活。只需要打开Unity工程运行，选择dropdownUI上的状态，并点击Trigger按钮，就能够触发对应的条件。
-状态机会在console上输出一些语句来表示当前的状态和切换情况。
 `homeworkProject-XLua`文件夹中是本次作业的第一题，仅实现了一个lua的hello world。
+`LuaHelloWorldBuildFile`是可执行文件的文件夹。
+在`LuaTest.cs`脚本内调用了`Resources->lua.lua.txt`脚本，在屏幕的ui上打出了`"Hello world!"`。
--- homework02/homeworkProject-XLua/Assets/LuaTest.cs
+++ homework02/homeworkProject-XLua/Assets/LuaTest.cs
@@ -1,21 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using XLua;
 
 public class LuaTest : MonoBehaviour
 {
+    public Text text;
     // Start is called before the first frame update
+    LuaEnv lua;
+    LuaFunction update;
+    LuaFunction start;
+    LuaFunction helloWorld;
+    LuaFunction onenable;
     void Start()
     {
-        LuaEnv luaEnv = new LuaEnv();
-        luaEnv.DoString("CS.UnityEngine.Debug.Log('Hello world!')");
-        luaEnv.Dispose();
+        lua = new LuaEnv();
+        lua.DoString("require'lua'");
+        update = lua.Global.Get<LuaFunction>("Update");
+        start = lua.Global.Get<LuaFunction>("Start");
+        helloWorld = lua.Global.Get<LuaFunction>("helloWorld");
+        start.Call();
+    }
+    public void LuaHelloWorld()
+    {
+        helloWorld.Call(text);
+    }
+    private void Update()
+    {
+        update.Call();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        lua.Dispose();
     }
 }
